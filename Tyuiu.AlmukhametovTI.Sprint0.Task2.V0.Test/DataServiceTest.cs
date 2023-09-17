098b6d78dedd0ceb046042fc176215483351b032
TimaTi1771 <talmuhametov@yandex.ru>
2023-09-17 16:57:26 +0300

    Task2

---
+++ Tyuiu.AlmukhametovTI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AlmukhametovTI.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.AlmukhametovTI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+
+            var name = "Тимур";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Тимур", res);
+
+        }
+    }
+}
